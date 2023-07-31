a581beddbe262e3a6d64ac73be5d16460f900f89
Zavorotniy Dmytro <121565594+ZavorotniyDmytro@users.noreply.github.com>
2023-07-31 16:30:34 +0300

    init

---
+++ src/main/java/com/codot/camundaconnectors/filestorage/FileServiceFunction.java
@@ -0,0 +1,15 @@
+package com.codot.camundaconnectors.filestorage;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileServiceFunction implements JavaDelegate {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileServiceFunction.class);
+
+	@Override
+	public void execute(DelegateExecution delegateExecution) throws Exception {
+
+	}
+}
