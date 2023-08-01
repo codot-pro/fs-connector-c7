6ff342a75a1438c4797a0e8263aba9c7c782f872
Zavorotniy Dmytro <121565594+ZavorotniyDmytro@users.noreply.github.com>
2023-08-02 02:26:43 +0300

    ADD: operation handler

--- src/main/java/com/codot/camundaconnectors/filestorage/FileServiceFunction.java
+++ src/main/java/com/codot/camundaconnectors/filestorage/FileServiceFunction.java
@@ -1,31 +1,48 @@
 package com.codot.camundaconnectors.filestorage;
 
-import org.camunda.bpm.engine.ExternalTaskService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-@Component
 public class FileServiceFunction implements JavaDelegate {
 	private static final Logger LOGGER = LoggerFactory.getLogger(FileServiceFunction.class);
-	private static final String result = null;
 
 	@Override
 	public void execute(DelegateExecution delegateExecution) throws Exception {
 		Properties properties = getProperties();
 		String url = properties.getProperty("url");
+		LOGGER.debug("url=" + url);
 
 		String operation = (String) delegateExecution.getVariable("operation");
+		LOGGER.debug("operation=" + operation);
 
-
-		
+		switch (operation) {
+			case "upload" -> {
+				String guid = FileOperation.upload(
+						url,
+						(String) delegateExecution.getVariable("filePath"),
+						(String) delegateExecution.getVariable("fileName"));
+				delegateExecution.setVariable("uploadResult", guid); // guid
+			}
+			case "get" -> {
+				String path = FileOperation.get(
+						url,
+						(String) delegateExecution.getVariable("guid"),
+						(String) delegateExecution.getVariable("filePath"));
+				delegateExecution.setVariable("getResult", path); // path
+			}
+			case "delete" -> {
+				String answer = FileOperation.delete(
+						url,
+						(String) delegateExecution.getVariable("guid"));
+				delegateExecution.setVariable("deleteResult", answer); // deleteResult
+			}
+			default -> throw new RuntimeException("Operation not found");
+		}
 	}
 
 	private static Properties getProperties() throws IOException {
@@ -34,8 +51,8 @@ public class FileServiceFunction implements JavaDelegate {
 
 		fis = new FileInputStream("src/main/resources/config.properties");
 		property.load(fis);
+		fis.close();
 
 		return property;
-
 	}
 }
